67fd6f93e55009a8adeffcde94bd1c5fceb47f62
ArcAngle <eddy@3os.mobi>
2022-04-21 13:10:59 +0200

    Full kode i ny forever loop

--- main.ts
+++ main.ts
@@ -15,3 +15,21 @@ basic.clearScreen()
 basic.forever(function () {
     WonderBit.Servo(servo1, servo1)
 })
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.soundLevel() >= 125) {
+        basic.showIcon(IconNames.Heart)
+        for (let index = 0; index < 6; index++) {
+            servo1 = 80
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(500)
+            servo1 = 10
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(500)
+        }
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        servo1 = 30
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
