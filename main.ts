ba07da16761c768d44e1ae167147ebac4cc07ee9
ArcAngle <eddy@3os.mobi>
2022-04-21 12:52:58 +0200

    Test med full "ved start"

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    servo1 = 80
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    servo1 = 10
+})
+let servo1 = 0
+WonderBit.Initialise()
+servo1 = 30
+basic.showIcon(IconNames.Yes)
+basic.pause(1000)
+basic.clearScreen()
+basic.forever(function () {
+    WonderBit.Servo(servo1, servo1)
+})
